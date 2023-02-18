cf55af0f745500f6c464571b00e0ceea290bf327
Bryan Caro <bryan_caro@carrefour.com>
2023-02-18 17:15:33 +0100

    Initial Commit

---
+++ CabifyChallenge/CabifyChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CabifyChallengeApp.swift
+//  CabifyChallenge
+//
+//  Created by Bryan Caro on 18/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CabifyChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
